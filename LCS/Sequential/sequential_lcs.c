a70169bfe6011694d2046ee27d364e79ddcdc9ed
Roberto Iuliano <94120694+iulianoroberto@users.noreply.github.com>
2025-04-08 18:34:59 +0200

    Update sequential_lcs.c

--- LCS/Sequential/sequential_lcs.c
+++ LCS/Sequential/sequential_lcs.c
@@ -35,6 +35,8 @@ unsigned short lcs(unsigned short *DP_matrix, char *A, char *B, int m, int n)
     return DP_matrix[m * (n + 1) + n];
 }
 
+// prova 
+
 int main(int argc, char *argv[])
 {
     // Check if the input file is provided
